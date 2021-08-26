736ff5780fcc51b61f0f8ce0e3d7cd1e1d965f21
Justin Katzwhite <jkatzwhite@gmail.com>
2021-08-26 00:04:13 -0400

    Layout, controller and theme.

--- app/src/main/java/com/my/microcenterbarcode/ui/main/MainFragment.kt
+++ app/src/main/java/com/my/microcenterbarcode/ui/main/MainFragment.kt
@@ -1,14 +1,24 @@
 package com.my.microcenterbarcode.ui.main
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.my.microcenterbarcode.R
 
+import kotlinx.android.synthetic.main.main_fragment_linear_layout.*
+
+import com.aspose.barcode.generation.BarcodeGenerator
+import com.aspose.barcode.*
+
 class MainFragment : Fragment() {
+
+    val userKey = "USER"
+    val pwKey = "PW"
 
     companion object {
         fun newInstance() = MainFragment()
@@ -20,7 +30,7 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(R.layout.main_fragment_linear_layout, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -30,4 +40,48 @@ class MainFragment : Fragment() {
         // TODO: Test
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val userName = activity?.getPreferences(Context.MODE_PRIVATE)?.getString(userKey, null) ?: userNameField.text.toString()
+        val pw = activity?.getPreferences(Context.MODE_PRIVATE)?.getString(pwKey, null) ?: passwordField.text.toString()
+        userNameField.setText(userName)
+        passwordField.setText(pw)
+//        if (userName != null && pw != null) {
+//            userNameField.setText(userName)
+//            passwordField.setText(pw)
+//        }
+        button.setOnClickListener(
+            View.OnClickListener { updateCredentials() }
+        )
+        generateBarcode(userName, pw)
+
+//        view.findViewById<ImageView>(R.id.floatingActionButton).setOnClickListener {
+//            viewModel.getUsers().observe(viewLifecycleOwner, Observer<List<String>>{ users ->
+//                view.findViewById<TextView>(R.id.message).text = users[0]
+//            })
+//        }
+//        view.findViewById<ImageView>(R.id.floatingActionButton).setOnClickListener {
+//            view.findViewById<TextView>(R.id.message).text = viewModel.getuserName()
+//        }
+
+
+    }
+
+    fun updateCredentials() {
+        val userName = userNameField.text.toString()
+        val pw = passwordField.text.toString()
+        activity?.getPreferences(Context.MODE_PRIVATE)?.edit()?.putString(userKey, userName)?.apply()
+        activity?.getPreferences(Context.MODE_PRIVATE)?.edit()?.putString(pwKey, pw)?.apply()
+        generateBarcode(userName, pw)
+    }
+
+    fun generateBarcode(userName: String, pw: String) {
+        val bitmap = BarcodeGenerator(EncodeTypes.CODE_128, userName).generateBarCodeImage()
+        userNameBarcode.setImageBitmap(
+            BarcodeGenerator(EncodeTypes.CODE_128, userName).generateBarCodeImage()
+        )
+        passwordBarcode.setImageBitmap(
+            BarcodeGenerator(EncodeTypes.CODE_128, pw).generateBarCodeImage()
+        )
+    }
 }
--- app/src/main/java/com/my/microcenterbarcode/ui/main/MainViewModel.kt
+++ app/src/main/java/com/my/microcenterbarcode/ui/main/MainViewModel.kt
@@ -1,7 +1,25 @@
 package com.my.microcenterbarcode.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val username: MutableLiveData<List<String>> by lazy {
+        MutableLiveData<List<String>>().also {
+            loadUser()
+        }
+
+    }
+
+    fun getUsers(): LiveData<List<String>> {
+        return username
+    }
+
+    private fun loadUser() {
+        username.value = listOf("First", "Second")
+    }
+    val userName = "test"
+    fun getuserName() : String{return userName}
 }
